56346d7e00ead8f48b39f08374b5d9f7fa1a9c14
JPAS3A <jpas3@aluno.ifal.edu.br>
2022-09-05 21:08:29 +0000

    soma_feita

---
+++ api/src/main/java/app/Soma.java
@@ -0,0 +1,13 @@
+package app;
+
+import io.jooby.annotations.*;
+
+@Path("/Soma/{op1}/{op2}")
+public class Soma {
+
+    @GET
+    public double calculaSoma(@PathParam double op1 , @PathParam double op2 ) {
+        double resultado = op1+op2;
+        return resultado;
+    }
+}
