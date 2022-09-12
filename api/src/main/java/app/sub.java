288556a73a71cc4c79c7dc9e19710569d113ecfb
marcosanb <mas55@aluno.ifal.edu.br>
2022-09-12 20:11:23 +0000

    linhas_removidas

--- api/src/main/java/app/sub.java
+++ api/src/main/java/app/sub.java
@@ -4,7 +4,6 @@ import io.jooby.annotations.*;
 
 @Path("/sub/{op1}/{op2}")
 public class sub {
-
     @GET
     public double calcularsub(@PathParam double op1 , @PathParam double op2) {
         double result = op1 - op2;
